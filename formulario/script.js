85ab4c6b10e2c1e6a799ad77dbda44c6c0d3fe73
Pedro-Nuniz <pedroon772@gmail.com>
2025-12-03 14:26:00 +0000

    changes

--- formulario/script.js
+++ formulario/script.js
@@ -1,44 +1,35 @@
- function validaForm() {
-            const email = document.getElementById('email');
-            let emailText = email.value.trim();
-            let valido = false;
-            if (emailText.includes("@")) {
-                valido = true; 
-            } else {
-                valido = false;
-            }
+function validaForm() {
+    const email = document.getElementById('email');
+    let emailText = email.value.trim();
+    let valido = true;
+    if (!emailText.includes("@")) {
+        valido = false;
+    }
 
-            let partesEmail = emailText.split("@");
-            if (partesEmail[0].lenght >= 7 && partesEmail[0].lenght <= 127) {
-                valido = true;
-            } else {
-                valido = false;
-            }
+    let partesEmail = emailText.split("@");
+    if (partesEmail[0].lenght < 7 && partesEmail[0].lenght > 127) {
+        valido = false;
+    }
 
-            const nome = document.getElementById("nome");
-            let nomeText = nome.value.trim();
-            if (nome.lenght >= 3) {
-                console.log("Nome  atende ao tamanho mínimo")
-                valido = true;
-            }
-            else {
-                valido = false;
-            }
+    const nome = document.getElementById("nome");
+    let nomeText = nome.value.trim();
+    if (nomeText.lenght < 3) {
+        console.log("Nome  atende ao tamanho mínimo")
+        valido = false;
+    }
 
-            const textArea = document.getElementById("mensagem");
-            let textTexto = textArea.value.trim();
-            if (textTexto !== "") {
-                     console.log("Texto não pode ser vazio!")
-                valido = true;
-            } else {
-                valido = false;
-            } 
+    const textArea = document.getElementById("mensagem");
+    let textTexto = textArea.value.trim();
+    if (textTexto === "") {
+        valido = false;
+        alert("Texto não pode ser vazio!")
+    }
+    if (valido)
+        alert("recebemos sua mensagem, em breve entraremos em contato")
+}
 
-            alert("recebemos sua mensagem, em breve entraremos em contato")
-        }
+document.addEventListener("submit", function (e) {
 
-        document.addEventListener("submit", function (e) {
-
-            e.preventDefault();
-            validaForm();
-        })
+    e.preventDefault();
+    validaForm();
+})
